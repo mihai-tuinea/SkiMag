93e442c4dc0d8d2f5fa152b89bb3892ec5640f8b
Mihai Tuinea <mihai.tuinea27@gmail.com>
2026-02-01 13:30:59 +0200

    basic routing setup

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
-import { ShopComponent } from './features/shop/shop.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
